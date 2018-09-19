8dfa81e12d4e392da886bae9daaf792ffde7187a
jwrigh26 <jwrigh26@gmail.com>
2018-09-18 23:43:30 -0600

    have sass modules working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import styles from "styles/App.scss";
 
 class App extends Component {
   render() {
+    console.log(styles);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <img src={logo} className={styles.logo} alt="logo" />
+          <h1 className={styles.app__title}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={styles.app__intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
