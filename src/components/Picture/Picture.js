11a1f18e026179995c278c1015618fb8a8f06975
jwrigh26 <jwrigh26@gmail.com>
2018-10-31 21:45:42 -0600

    Picture component

--- src/components/Picture/Picture.js
+++ src/components/Picture/Picture.js
@@ -1,8 +1,83 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import css from './picture.scss';
+
+const req = require.context('assets/images', true);
+
+const renderWebpSource = (image, size, width) => {
+  if (size && width) {
+    try {
+      const src1 = req(`./${image}-${size}_1x.webp`);
+      const src2 = req(`./${image}-${size}_2x.webp`);
+      return (
+        <source
+          media={`(min-width: ${width}px)`}
+          srcSet={`${src1} 1x, ${src2} 2x`}
+          type="image/webp"
+          alt={`image-${image}`}
+        />
+      );
+    } catch (error) {
+      const orig = req(`./${image}.webp`);
+      return <source srcSet={`${orig}`} type="image/webp" alt={`${error}`} />;
+    }
+  }
+
+  const src1 = req(`./${image}-sm_1x.webp`);
+  const src2 = req(`./${image}-sm_2x.webp`);
+  return <source srcSet={`${src1} 1x, ${src2} 2x`} type="image/webp" alt={`image-${image}`} />;
+};
+
+const renderJpgSource = (image, size, width) => {
+  if (size && width) {
+    try {
+      const src1 = req(`./${image}-${size}_1x.jpg`);
+      const src2 = req(`./${image}-${size}_2x.jpg`);
+      return (
+        <source
+          media={`(min-width: ${width}px)`}
+          srcSet={`${src1} 1x, ${src2} 2x`}
+          type="image/jpeg"
+          alt={`image-${image}`}
+        />
+      );
+    } catch (error) {
+      const orig = req(`./${image}.jpg`);
+      return <source srcSet={`${orig}`} type="image/jpeg" alt={`${error}`} />;
+    }
+  }
+
+  const src1 = req(`./${image}-sm_1x.jpg`);
+  const src2 = req(`./${image}-sm_2x.jpg`);
+  return <source srcSet={`${src1} 1x, ${src2} 2x`} type="image/jpeg" alt={`image-${image}`} />;
+};
+
+const renderImage = image => {
+  try {
+    const src1 = req(`./${image}-sm_1x.jpg`);
+    const src2 = req(`./${image}-md_1x.jpg`);
+    const src3 = req(`./${image}-lg_1x.jpg`);
+    const srcSet = `${src1} 600w, ${src2} 900w, ${src3} 1440w`;
+    const src = src1;
+    return <img srcSet={srcSet} src={src} type="image/jpeg" alt={`image-${image}`} />;
+  } catch (error) {
+    const orig = req(`./${image}.jpg`);
+    return <img src={orig} type="image/jpeg" alt={`${error}`} />;
+  }
+};
 
 const Picture = ({image}) => {
-  return <div />;
+  return (
+    <picture className={css.picture}>
+      {renderWebpSource(image, 'lg', 900)}
+      {renderWebpSource(image, 'md', 601)}
+      {renderWebpSource(image)}
+      {renderJpgSource(image, 'lg', 900)}
+      {renderJpgSource(image, 'md', 601)}
+      {renderJpgSource(image)}
+      {renderImage(image)}
+    </picture>
+  );
 };
 
 Picture.defaultProps = {
--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import css from './home.scss';
 
+import Picture from 'components/Picture/Picture';
+
+// const pathToImage = require('neptune_02-lg_1x.jpg');
+
+// const image1 = 'assets/images/neptune_01-lg_1x.jpg';
+// const image2 = 'static/images/neptune_02';
+
 const Home = props => {
   return (
     <div className={css.wrapper}>
       <div className={css.header} />
-      <div className={css.comic} />
-      <div className={css.art} />
+      <div className={css.comic}>
+        <Picture image={'koen_asobi'} />
+      </div>
+      <div className={css.art}>
+        <Picture image={'neptune_03'} />
+      </div>
       <div className={css.social} />
       <div className={css.shop} />
     </div>
