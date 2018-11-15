3f0df4d86f4413f0ded49cb69e12eb7c3ff97f2c
jwrigh26 <jwrigh26@gmail.com>
2018-11-14 20:47:16 -0700

    Worked on drawer animation

--- src/components/Header/Drawer.js
+++ src/components/Header/Drawer.js
@@ -7,21 +7,40 @@ import close from 'assets/icons/close.json';
 import {drawerState, isDrawerHidden, isDrawerOpen} from './header.helper';
 import css from './drawer.scss';
 
+const inTrans = {type: 'tween', duration: 300, ease: 'circIn'};
+const outTrans = {type: 'tween', duration: 425, ease: 'circOut'};
 const Box = posed.div({
   open: {
     translateX: 0,
-    transition: {type: 'tween', duration: 425, ease: 'circOut'},
+    transition: outTrans,
   },
 
   closed: {
-    translateX: '70vw',
-    transition: {type: 'tween', duration: 425, ease: 'circIn'},
+    delay: 150,
+    translateX: '68vw',
+    transition: inTrans,
+  },
+});
+
+const Close = posed.div({
+  open: {
+    delay: 225,
+    translateX: 0,
+    rotate: 0,
+    opacity: 1,
+    transition: outTrans,
+  },
+  closed: {
+    translateX: '81vw',
+    rotate: 360,
+    opacity: 0,
+    transition: inTrans,
   },
 });
 
 const Shade = posed.div({
-  open: {opacity: 1},
-  closed: {opacity: 0},
+  open: {opacity: 1, transition: outTrans},
+  closed: {opacity: 0, delay: 150, transition: inTrans},
 });
 
 const Drawer = ({children, drawerPosition, setDrawerPosition, toggleDrawer}) => {
@@ -35,13 +54,7 @@ const Drawer = ({children, drawerPosition, setDrawerPosition, toggleDrawer}) => 
   };
 
   const renderTopNav = () => {
-    return (
-      <div className={css.topNav}>
-        <Button onClick={toggleDrawer}>
-          <Icon svg={close} className={[css.closeButton]} />
-        </Button>
-      </div>
-    );
+    return <div className={css.topNav} />;
   };
 
   return (
@@ -51,18 +64,23 @@ const Drawer = ({children, drawerPosition, setDrawerPosition, toggleDrawer}) => 
         initialPose={'closed'}
         key="shade"
         onClick={toggleDrawer}
-        pose={pose}
-      />
-      <Box
-        className={[css.box, hidden].join(' ')}
-        initialPose={'closed'}
-        key="box"
         onPoseComplete={handlePoseComplete(drawerPosition, setDrawerPosition)}
         pose={pose}
-      >
+      />
+      <Box className={[css.box, hidden].join(' ')} initialPose={'closed'} key="box" pose={pose}>
         {renderTopNav()}
         {children}
       </Box>
+      <Close
+        className={[css.close, hidden].join(' ')}
+        initialPose={'closed'}
+        key="close"
+        pose={pose}
+      >
+        <Button className={[css.closeButton]} onClick={toggleDrawer}>
+          <Icon svg={close} className={[css.closeIcon]} />
+        </Button>
+      </Close>
     </div>
   );
 };
