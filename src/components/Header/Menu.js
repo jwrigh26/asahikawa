30d92edabd697aa6cf5b55d9ca60ad65e23d5f1f
juwright <jwright@swipeclock.com>
2019-01-05 12:03:13 -0700

    Added mobile style for drawer menu

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -7,15 +7,16 @@ const Menu = ({navigation, toggleDrawer}) => {
   const {paths, rootPath} = navigation;
   console.log(paths);
   return (
-    <nav>
-      <span>Menu</span>
-      <ul>
+    <nav className={css.nav}>
+      <span className={css.nav__title}>Menu</span>
+      <ul className={css.menu}>
         {paths.map(e => (
-          <li key={e.key}>
+          <li className={css.menu__item} key={e.key}>
             <NavLink
+              activeClassName={css.selected}
               to={`${rootPath}#${e.title}`}
               onClick={toggleDrawer}
-              activeClassName={css.selected}
+              location={{pathname: document.location.pathname + document.location.hash}}
             >
               {e.title}
             </NavLink>
